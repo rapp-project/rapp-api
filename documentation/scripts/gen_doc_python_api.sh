c2b17aefa6b87b98d6c0fdb9da0e76c5c22eff59
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-12-05 19:29:09 +0200

    Python API doc-generation script

---
+++ documentation/scripts/gen_doc_python_api.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
+
+doxy_conf_file="${CURRENT_DIR}/../doxy_conf/doxy_python_api.conf"
+out_dir_default="${HOME}/rapp_platform_files/documentation/rapp_api/python"
+
+if [ -z "$1" ]; then
+  out_dir=${out_dir_default}
+else
+  out_dir="$1"
+fi
+
+# Create output directory if it does not exist
+mkdir -p ${out_dir}
+
+match_str="OUTPUT_DIRECTORY"
+append_str="${match_str} = ${out_dir}"
+
+# Remove match_str line from doxy_conf_file
+sed -i "/${match_str}/d" ${doxy_conf_file}
+
+grep -q "${match_str}" "${doxy_conf_file}" || \
+  echo -e "${append_str}" >> ${doxy_conf_file}
+
+# Redirect stdout.
+doxygen ${doxy_conf_file} 1> /dev/null
