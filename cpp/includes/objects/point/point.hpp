ff7a6c27ef70fcf11c69224e169e4f66e8af8390
Alex Ge <alexge233@hotmail.com>
2016-06-19 23:57:59 +0100

    added new object structs from wut_devel

---
+++ cpp/includes/objects/point/point.hpp
@@ -0,0 +1,55 @@
+#ifndef RAPP_OBJECT_POINT
+#define RAPP_OBJECT_POINT
+namespace rapp {
+namespace object {
+/**
+ * \struct point
+ * \brief encapsulate point position vector
+ * \version 0.6.0
+ * \date 10-May-2016
+ * \author Wojciech Dudek <dudekwa@gmail.com>
+ */
+struct point
+{
+    /**
+     * \brief Consruct using code coordinates (x,y,z)
+     * \param x is x coordinate
+     * \param y is y coordinate
+     * \param z is z coordinate
+     */
+    point(float x, float y, float z)
+    : x(x), y(y), z(z)
+    {}
+    
+    /// \brief Allow Empty Consructor
+    point() = default;
+    
+    /// \brief Copy Conatructor
+    point(const rapp::object::point &) = default;
+    
+    /// \brief Equality operator
+    bool operator==(const rapp::object::point & rhs) const
+    {
+		return (this->x == rhs.x) &&
+			   (this->y == rhs.y) &&
+			   (this->z == rhs.z);
+    }
+
+	/// \brief convert *this* into a boost tree
+	boost::property_tree::ptree treefy() const
+	{
+		boost::property_tree::ptree tree;
+        tree.put("x", x);
+        tree.put("y", y);
+		tree.put("z", z);
+		return tree;
+	}
+
+	/// members
+	float x = 0;
+    float y = 0;
+    float z = 0;
+};
+}
+}
+#endif
---
+++ cpp/includes/objects/pose/pose.hpp
@@ -0,0 +1,55 @@
+#ifndef RAPP_OBJECT_POSE
+#define RAPP_OBJECT_POSE
+#include "includes.ihh"
+namespace rapp {
+namespace object {
+/**
+ * \struct pose
+ * \brief encapsulates robot pose vectors (position & orientation)
+ * \version 0.6.0
+ * \date 10-May-2016
+ * \author Wojciech Dudek <dudekwa@gmail.com>
+ */
+struct pose
+{
+    /**
+     * \brief Consruct using position and orientation components
+     * \param position is vector of position coordinates
+     * \param orientation is vector of orientation (quaternion) coordinates
+     */
+    pose(
+           point position,
+           quaternion orientation
+		)
+    : position(position), orientation(orientation)
+    {}
+    
+    /// \brief allow empty consructor
+    pose() = default;
+    
+    /// \brief copy constructor
+    pose(const rapp::object::pose &) = default;
+    
+    /// \brief Equality operator
+    bool operator==(const pose & rhs) const
+    {
+		return (this->position == rhs.position) &&
+			   (this->orientation == rhs.orientation);
+    }
+
+	/// \brief convert *this* into a boost tree
+	boost::property_tree::ptree treefy() const
+	{
+		boost::property_tree::ptree tree;
+        tree.add_child("position", position.treefy());
+		tree.add_child("orientation", orientation.treefy());
+		return tree;
+	}
+
+	/// members
+	rapp::object::point position;
+	rapp::object::quaternion orientation;
+};
+}
+}
+#endif
