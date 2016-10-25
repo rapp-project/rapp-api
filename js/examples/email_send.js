cfc4b76e02e6a55a003dc2f71b45fe75691a2325
lpenteri <lp@ortelio.co.uk>
2016-10-25 23:25:47 +0300

    changed the examples to find the path of each call relatively to rapp-platform-api as an npm package

--- js/examples/email_send.js
+++ js/examples/email_send.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // Import the email_send JS API Service & Init the RAPPCloud Object
-var RAPPCloud = require('rapp-platform-api/includes/cloud/RAPPCloud');
-RAPPCloud.email_send = require('rapp-platform-api/includes/cloud/email_send');
+var RAPPCloud = require('rapp-platform-arapp-platform-api/includes/cloud/RAPPCloud');
+RAPPCloud.email_send = require('rapp-platform-arapp-platform-api/includes/cloud/email_send');
 
 var services = new RAPPCloud( );
 
--- js/examples/upload_map.js
+++ js/examples/upload_map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // Import the path_planning_upload_map JS API Services & Init the RAPPCloud Object
-var RAPPCloud = require('rapp-platform-api/includes/cloud/RAPPCloud');
-RAPPCloud.path_planning_upload_map = require('rapp-platform-api/includes/cloud/path_planning_upload_map');
+var RAPPCloud = require('rapp-platform-arapp-platform-api/includes/cloud/RAPPCloud');
+RAPPCloud.path_planning_upload_map = require('rapp-platform-arapp-platform-api/includes/cloud/path_planning_upload_map');
 
 var services = new RAPPCloud( );
 
